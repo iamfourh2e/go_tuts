b4d95ef4d3e8765b8a76c91292e451b758ccfe11
iamfourh2e <iamfourh2e@gmail.com>
2025-05-09 22:28:42 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+	//ប្រកាសជាអក្សរ
+	var a string = "Hello world"
+	//ប្រកាសជាលេខគត់
+	var b int = 10
+	//ប្រកាសជាលេខមានក្បៀស
+	var c float32 = 3.14
+	//ប្រកាសជា ឈ្នាប់ពិតឬមិនពិត
+	var d bool = true
+	//ប្រកាសខ្លីៗដែល go និងស្គាល់ប្រភេទទិន្នន័យដោយស្វ័យប្រវត្តិ
+	e := "short declaration"
+	//បង្ហាញទិន្នន័យនៅក្នុង console
+	println(a, b, c, d, e)
+}
